46c1a414997b24ef836b51d0ebe034723aac6e19
Luiz Henrique da Silva <henriquesilva.luiz@gmail.com>
2018-10-20 21:28:13 -0300

    Criação das telas iniciais do front end e das controllers.

--- ControleClientes/App_Start/BundleConfig.cs
+++ ControleClientes/App_Start/BundleConfig.cs
@@ -14,6 +14,12 @@ namespace ControleClientes
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jsGrid").Include(
+                        "~/Scripts/jsgrid.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/clientes").Include(
+                        "~/Scripts/clientes.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,6 +31,10 @@ namespace ControleClientes
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/jsGrid").Include(
+                "~/Scripts/jsgrid.css",
+                "~/Scripts/jsgrid-theme.css"));
         }
     }
 }
---
+++ ControleClientes/Controllers/TipoContaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleClientes.Controllers
+{
+    public class TipoContaController : Controller
+    {
+        // GET: TipoConta
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
